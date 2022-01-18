b02021c6a4d34187c5863b2d67b3f8e169d4f019
Your Name <you@example.com>
2022-01-18 23:58:53 +0800

    git rarity list

---
+++ getRarityList.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+def getListResponse():
+    try:
+        url = 'https://graph.compass.art/graphql'
+        body = {
+            "operationName": "GetTrendingCollections",
+            "variables": {
+                "period": "1h",
+                "sortBy": "salesCount",
+                
+                "take": 20,
+                "skip": 0
+            },
+            "query": "query GetTrendingCollections($period: String, $sortBy: String, $sortDesc: Boolean, $take: Int, $skip: Int) {\n  collections: trendingCollections(\n    period: $period\n    sortBy: $sortBy\n    sortDesc: $sortDesc\n    take: $take\n    skip: $skip\n  ) {\n    id\n    collection {\n      id\n      name\n      isVerified\n      imageUrl\n      totalSupply\n      slug\n      salesFloor\n      previousSalesFloor\n      contractCreatedTimestamp\n      charts(period: $period) {\n        volume {\n          key\n          value\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    volume\n    salesCount\n    avgPrice\n    maxPrice\n    medianPrice\n    previous {\n      volume\n      salesCount\n      avgPrice\n      minPrice\n      maxPrice\n      medianPrice\n      __typename\n    }\n    __typename\n  }\n}\n"
+        }
+        header = {
+            "Content-Type": "application/json",
+        }
+        res = requests.post(url=url ,headers=header, data=json.dumps(body))
+    except Exception as e:
+        print('error')
+        print(e)
+        pass
+
+    return json.loads(res.content)
+
+def getContractAddress(list_response):
+    result = []
+    for object in list_response['data']['collections']:
+        result.append(object['id'])
+    
+    return result
+
+if __name__ == "__main__":
+    list_response = getListResponse()
+    contract_address = getContractAddress(list_response)
+    print(contract_address)
